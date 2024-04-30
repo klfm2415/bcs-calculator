6f46249a45a9529fc78af7e8545af2310442d002
klfm2415 <klfm241500@gmail.com>
2024-04-30 13:49:46 +0900

    onclicknumber

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ let result = 0;
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+
+  console.log(resultEl.innerHTML.length);
+}
